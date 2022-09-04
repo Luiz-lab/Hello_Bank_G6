9ed8fcf2af8dbe3b05ce7151d8f7c612320fbee9
Luiz-lab <calixtoluiz0@gmail.com>
2022-09-04 12:46:54 -0300

    Init Projetct

---
+++ src/test/java/com/br/g6bank/hellobank/HellobankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.g6bank.hellobank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellobankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
