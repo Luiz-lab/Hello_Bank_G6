fc077b5591353bc2f7d650c86ac3ef1f2f4d2238
Isaias <99995191+isaias30silva@users.noreply.github.com>
2022-09-08 13:07:54 -0300

    Create DefaultRouteController.java

---
+++ src/main/java/ibm/ibtc/Hello_bank_G6/Controllers/DefaultRouteController.java
@@ -0,0 +1,29 @@
+package ibm.ibtc.Hello_bank_G6.Controllers;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("/")
+public class DefaultRouteController {
+
+//    @GetMapping("/")
+//    public ModelAndView getAll() {
+//        return new ModelAndView("redirect:" + "https://google.com");
+//    }
+
+//    @GetMapping("/")
+//    public ResponseEntity<Object> getAll() {
+//        return ResponseEntity.status(HttpStatus.CREATED).body(new Object() {
+//            public final Object message = "message";
+//        });
+//    }
+
+    @GetMapping("/")
+    public ModelAndView welcome() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index.html");
+        return modelAndView;
+    }
+
+}
